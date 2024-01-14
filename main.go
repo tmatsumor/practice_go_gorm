0f5e55d06e11fc06cf1ab57cfaf522afac741f9a
t.matsumori <t.matsumori@visionary-jp.net>
2024-01-14 22:43:06 +0900

    add delete

--- main.go
+++ main.go
@@ -23,26 +23,28 @@ func main() {
 
 	// GORMを使ったデータベース操作をここに記述
 	/*
+		// create user
 		user := User{Name: "John Doe", Email: "joen@example.com"}
 		result := db.Create(&user)
 		if result.Error != nil {
 			panic("failed to insert record")
 		}
 	*/
+	// select user
 	var user User
 	db.First(&user)
 	db.Where("name=?", "John Doe").Find(&user)
 	var users []User
 	db.Find(&users)
 
+	// update user
 	user.Name = "Jane Doe"
 	db.Save(&user)
 
 	db.Model(&user).Update("email", "hoge@example.com")
 
 	db.Model(&user).Updates(map[string]interface{}{"name": "Jane DDD", "email": "foo@example.com"})
-}
 
-func Create() {
-
+	// delete user
+	db.Where("email=? AND name=?", "foo@example.com", "Jane DDD").Delete(&User{})
 }
